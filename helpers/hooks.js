54dd08870d393b3e809548961d63ab7a30e9e747
Samuel Leite <samuel-leite@outlook.com.br>
2023-12-16 11:58:52 -0300

    refactor(all): 💡 refactoring conf
    refacotitoring config

--- helpers/hooks.js
+++ helpers/hooks.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 const Helper = require('@codeceptjs/helper')
-const { container, ecorder, event, output, helper } = require('codeceptjs')
-
-const rimraf = require('rimraf')
+const { container, output, helper } = require('codeceptjs')
+const execSync = require('child_process').execSync
+const utf8 = { encoding: 'utf-8' }
 
 class hooks extends Helper {
   _init() {
@@ -14,7 +14,7 @@ class hooks extends Helper {
     console.log('*************************************')
     console.log('** Limpeza do output de evidências **')
     try {
-      rimraf.sync('output/')
+      execSync('rm -rfv output/', utf8)
       console.log('Diretório "output" excluído com sucesso!')
     } catch (error) {
       console.error(`Ocorreu um erro ao excluir o diretório: ${error}`)
@@ -22,40 +22,32 @@ class hooks extends Helper {
     console.log('*************************************')
   }
 
-  // _before(test) {
-  // before a test
-  // codeceptjs.container
-  //   .helpers()
-  //   .Playwright.browserContext.setGeolocation({ latitude: -23.558, longitude: -46.6597 })
-  // codeceptjs.container.helpers().Playwright.browserContext.grantPermissions(['geolocation'])
-  // test.retries(process.env.RETRY)
-  // console.log('--------------------------------Start----------------------------------')
-  // const allure = codeceptjs.container.plugins('allure')
-  // allure.addParameter('0', 'ENV', process.env.BROWSER)
-  // }
-
-  // _after() {
-  //   console.log('--------------------------------End----------------------------------')
-  // } // after a test
-
-  // _beforeStep() {} // before each step
-
-  // _afterStep() {
-  //   // after each step
-  //   utils.addEvidenciaApiAllure()
-  // }
-
-  // _beforeSuite() {} // before each suite
-
-  // _afterSuite() {} // after each suite
-
-  // _passed() {} // after a test passed
-
-  // _failed() {} // after a test failed
-
-  // _finishTest() {
-  //   // after all tests
-  //   //  execSync('allure serve output', utf8)
-  // }
+  _before(test) {
+    // before a test
+    codeceptjs.container
+      .helpers()
+      .Playwright.browserContext.setGeolocation({ latitude: -23.558, longitude: -46.6597 })
+    codeceptjs.container.helpers().Playwright.browserContext.grantPermissions(['geolocation'])
+    test.retries(process.env.RETRY)
+    console.log('--------------------------------Start----------------------------------')
+    // let allure = codeceptjs.container.plugins('allure')
+    // allure.addParameter('0', 'ENV', process.env.BROWSER)
+  }
+  _after() {
+    // after a test
+    console.log('--------------------------------End----------------------------------')
+  }
+  _beforeStep() {} // before each step
+  _afterStep() {
+    // after each step
+  }
+  _beforeSuite() {} // before each suite
+  _afterSuite() {} // after each suite
+  _passed() {} // after a test passed
+  _failed() {} // after a test failed
+  _finishTest() {
+    // after all tests
+     execSync('allure serve output', utf8)
+  }
 }
 module.exports = hooks
