f3d6bf95ca7a096de2b3a43d20546183f45a1c92
Samuel Leite <samuel-leite@outlook.com.br>
2023-12-18 22:31:57 -0300

    refactor(all): 💡 page refactoring
    page refactoring

--- tests/pages/StudentPage.js
+++ tests/pages/StudentPage.js
@@ -6,6 +6,18 @@ const navbar = require('./components/Navbar')
 const popup = require('./components/Popup')
 const notification = require('./components/Notification')
 
+const locs = {
+  txtName: '#name',
+  txtEmail: '#email',
+  txtAge: '#age',
+  txtWeight: '#weight',
+  txtFeetTall: '#feet_tall',
+  txtSearch: 'input[placeholder="Buscar por nome"]',
+  btnCadastrar: 'Cadastrar',
+  btnRemove: (email) => `//td[text()="${email}"]/..//button`,
+  lblMessagemAlerta: (label) => `//label[text()="${label}"]/..//span`
+}
+
 module.exports = {
   // versão alternativa do constructor em Java referenciando os componentes
   navbar,
@@ -13,28 +25,28 @@ module.exports = {
   notification,
 
   goToRegister() {
-    I.click('Cadastrar')
+    I.click(locs.btnCadastrar)
   },
 
   submitForm(student) {
-    if (student.name) I.fillField('#name', student.name)
-    if (student.email) I.fillField('#email', student.email)
-    if (student.age) I.fillField('#age', student.age)
-    if (student.weight) I.fillField('#weight', student.weight)
-    if (student.feet_tall) I.fillField('#feet_tall', student.feet_tall)
-    I.click('Cadastrar')
+    if (student.name) I.fillField(locs.txtName, student.name)
+    if (student.email) I.fillField(locs.txtEmail, student.email)
+    if (student.age) I.fillField(locs.txtAge, student.age)
+    if (student.weight) I.fillField(locs.txtWeight, student.weight)
+    if (student.feet_tall) I.fillField(locs.txtFeetTall, student.feet_tall)
+    I.click(locs.btnCadastrar)
   },
 
   alertMessage(label, text) {
-    I.see(text, `//label[text()="${label}"]/..//span`)
+    I.see(text, locs.lblMessagemAlerta(label))
   },
 
   search(name) {
-    I.fillField('input[placeholder="Buscar por nome"]', name)
+    I.fillField(locs.txtSearch, name)
   },
 
   remove(email) {
-    I.waitForElement(`//td[text()="${email}"]/..//button`);
-    I.click(`//td[text()="${email}"]/..//button`)
+    I.waitForElement(locs.btnRemove(email));
+    I.click(locs.btnRemove(email))
   }
 }
